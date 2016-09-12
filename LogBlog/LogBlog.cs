e305d15a86f9100170858606428ffb2d43e2a573
cden <cden@DESKTOP-NFUNKM2>
2016-09-12 22:36:36 +0100

    Add project files.

---
+++ LogBlog/LogBlog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logblog_console
+{
+    class LogBlog
+    {
+        static void Main(string[] args)
+        {
+            String logAddress;
+
+
+            //Console.SetWindowSize(84, 84);
+            //Console.SetWindowPosition(50, 50);
+
+            Console.WriteLine("Please point me to the log");
+
+            //logAddress = Console.ReadLine();
+            logAddress = "D:\\Users\\colin.denny\\Desktop\\LogBlog\\SWIM.JobServices.RetailWindowsService.2016.09.05.log";
+
+            Console.WriteLine("Reading Log: " + logAddress);
+
+            LogReader newLog = new LogReader(logAddress);
+
+            Console.WriteLine("Please enter any key to close");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LogBlog/LogReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class LogReader
+{
+    public LogReader(String LogAddress)
+	{
+        // Read each line of the file into a string array. Each element
+        // of the array is one line of the file.
+        string[] lines = System.IO.File.ReadAllLines(@LogAddress);
+
+        // Display the file contents by using a foreach loop.
+        System.Console.WriteLine("Log Contents:" + "\n");
+        foreach (string line in lines)
+        {
+            if (line.ToLower().Contains("exception") || line.ToLower().Contains("error"))
+            {
+                //Get the line number by using the array index
+                Int32 LineNo = Array.IndexOf(lines, line) +1;
+
+                // Use a tab to indent each line of the file.
+                Console.WriteLine("Line #" + LineNo + " ----- " + line + "\n");
+            }
+        }
+	}
+}
